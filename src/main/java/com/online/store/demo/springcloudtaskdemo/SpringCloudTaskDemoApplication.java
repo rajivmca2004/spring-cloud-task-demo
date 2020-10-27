4a1e6cceb97155ffc83eddb0cff5ec2281436586
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-10-27 14:48:29 +0530

    first commit

---
+++ src/main/java/com/online/store/demo/springcloudtaskdemo/SpringCloudTaskDemoApplication.java
@@ -0,0 +1,13 @@
+package com.online.store.demo.springcloudtaskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskDemoApplication.class, args);
+	}
+
+}
